ae9ae0c87b691509af5d381cce7713a09247a184
bando <bando7507@gmail.com>
2022-08-18 17:13:44 +0100

    finissions de la section qualification

--- app.js
+++ app.js
@@ -17,3 +17,46 @@ allLinks.forEach(el =>{
     })
 })
 
+
+
+// const a = document.querySelectorAll('#dd')
+const aID = document.querySelectorAll('#dd')
+const skillHeader= document.querySelectorAll('.skills_header')
+
+skillHeader.forEach(el =>{
+    el.addEventListener('click', () =>{
+        for(i = 0; i  < aID.length; i++){
+            const itemClass = aID[i].className
+            // console.log(itemClass)
+
+            if(itemClass ==='skills__content skills__open'){
+                aID[i].className = 'skills__content skills__close'
+            }else{
+                aID[i].className = 'skills__content skills__open'
+            }    
+        }      
+     })
+})
+
+
+
+const Tabs = document.querySelectorAll('[data-target]'),
+     tabsContent = document.querySelectorAll('[data-content]')
+
+
+     Tabs.forEach(el =>{
+        el.addEventListener('click', ()=>{
+             const tar = document.querySelector(el.dataset.target) 
+            
+         tabsContent.forEach(items =>{
+             items.classList.remove('qualification__active')
+         } )
+         tar.classList.add('qualification__active')
+
+         el.classList.remove('qualification__active')
+         
+        })
+     })
+
+
+
